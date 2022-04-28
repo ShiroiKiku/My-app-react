c9ba4b72538270445634a3fe80bc58b3656c2f31
ShiroiKiku <ividevil@mail.ru>
2022-04-28 17:37:53 +0400

    master

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import Counter from "./components/Counter"
+import PostItem from "./components/PostItem"
+import "./styles/App.css"
 
 function App() {
+  const [posts, setPosts] = useState([
+    { id: 1, title: "JavaScript", body: "Description" },
+    { id: 2, title: "JavaScript 2", body: "Description" },
+    { id: 3, title: "JavaScript 3", body: "Description" },
+    { id: 4, title: "JavaScript 4", body: "Description" },
+  ])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Counter /> */}
+      <PostItem post={{ id: 1, title: "JavaScript", body: "Description" }} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
