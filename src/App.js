5af108940321518b83fdf665c35bdd35a93cb75f
ShiroiKiku <ividevil@mail.ru>
2022-04-29 15:05:17 +0400

    master

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react"
 import Counter from "./components/Counter"
 import PostItem from "./components/PostItem"
+import PostList from "./components/PostList"
+import MyButton from "./components/UI/button/MyButton"
+import MyInput from "./components/UI/input/MyInput"
 import "./styles/App.css"
 
 function App() {
@@ -10,11 +13,17 @@ function App() {
     { id: 3, title: "JavaScript 3", body: "Description" },
     { id: 4, title: "JavaScript 4", body: "Description" },
   ])
+  const [title, setTitle] = useState("")
+  const addNewPost = () => {}
 
   return (
     <div className="App">
-      {/* <Counter /> */}
-      <PostItem post={{ id: 1, title: "JavaScript", body: "Description" }} />
+      <form>
+        <MyInput value={title} type="text" placeholder="Post name" />
+        <MyInput type="text" placeholder="Post description" />
+        <MyButton onClick={addNewPost}>Post create</MyButton>
+      </form>
+      <PostList posts={posts} title="Title List" />
     </div>
   )
 }
